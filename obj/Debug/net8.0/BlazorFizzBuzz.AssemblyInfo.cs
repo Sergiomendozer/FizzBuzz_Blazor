84c24a040bab78cae6ce20f4b3833097fc3e971a
Sergio Mendoza <sergiomendozansx@gmail.com>
2023-12-30 17:23:38 -0800

    buzzclass can be null

--- obj/Debug/net8.0/BlazorFizzBuzz.AssemblyInfo.cs
+++ obj/Debug/net8.0/BlazorFizzBuzz.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("BlazorFizzBuzz")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e88600a3e0e1aff31b982eaf615bb36e6d0e9c7e")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+7a89162496ed4c41ad5732e13435f76652c031f1")]
 [assembly: System.Reflection.AssemblyProductAttribute("BlazorFizzBuzz")]
 [assembly: System.Reflection.AssemblyTitleAttribute("BlazorFizzBuzz")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
